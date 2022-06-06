504a9dd2f2933965f8a19fa420bca08c7759d614
Sivamani <5maurosivamani@gmail.com>
2022-06-06 14:16:17 +0530

    completed the ejs blog post app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { redirect } = require("express/lib/response");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,9 +20,10 @@ app.use(express.static("public"));
 const posts = [];
 
 app.get("/", (req, res)=>{
+
   res.render("home", {
     startingContent: homeStartingContent, 
-    posts: posts
+    posts: posts 
   })
 })
 
@@ -35,6 +37,19 @@ app.get("/contact", (req, res)=>{
 
 app.get("/compose", (req, res)=>{
   res.render("compose")
+})
+
+app.get("/posts/:postTitle", (req, res)=>{
+  const requestedTitle = _.lowerCase(req.params.postTitle) 
+
+  posts.forEach((post)=>{
+    const storedTitle = _.lowerCase(post.title);
+
+    if(requestedTitle === storedTitle){
+      res.render("post",{ post: post })
+    }
+  })
+
 })
 
 app.post("/compose", (req, res)=>{
